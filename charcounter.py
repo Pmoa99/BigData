4f35d93d16baaaf55019497d2734645e359a2d20
Pmoa99 <75099272+Pmoa99@users.noreply.github.com>
2022-02-12 20:40:13 +0000

    Add files via upload

---
+++ charcounter.py
@@ -0,0 +1,59 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import sys"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 14,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "19\n"
+     ]
+    }
+   ],
+   "source": [
+    "#!/usr/bin/python\n",
+    "count = 0\n",
+    "TAB_CHAR = 't'\n",
+    "for line in sys.stdin:\n",
+    "    for token in line.strip().split(\"\\n\"):\n",
+    "        if token:\n",
+    "            count += 1\n",
+    "            \n",
+    "print(count)\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
