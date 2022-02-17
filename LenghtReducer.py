0c262d82050880bbd65175c02610067e08ff85f8
Pmoa99 <75099272+Pmoa99@users.noreply.github.com>
2022-02-17 14:57:59 +0000

    Create LenghtReducer.py

---
+++ LenghtReducer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import sys
+
+
+TAB_CHAR = '\t'
+
+lastKey = None
+a = 0
+s = 0
+count = 0
+total = 0
+
+for line in sys.stdin:
+    key, value = line.strip().split(TAB_CHAR)
+
+    if lastKey and lastKey != key:
+        print(lastKey + TAB_CHAR + 'Number char:' + str(a) + TAB_CHAR + 'Number words:' + str(s))
+        lastKey = key
+        a = len(key)
+        s = int(value)
+    else:
+        lastKey = key
+        a = len(str(key))
+        s += int(value)
+    
+    count +=1
+
+    
+
+if lastKey:
+    total = str(a)/str(s)
+    print(lastKey + TAB_CHAR + 'Average Lenght:' + str(total))
